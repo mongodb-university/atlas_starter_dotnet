e949d5aec24a610105618316871563b6303c2363
nate contino <nathan.contino@mongodb.com>
2020-07-01 14:21:32 -0600

    Altas -> Atlas typo fix

--- AtlasStarter/Program.cs
+++ AtlasStarter/Program.cs
@@ -10,7 +10,7 @@ namespace AtlasStarter
         {
             // TODO:
             // Replace the placeholder connection string below with your
-            // Altas cluster specifics. Be sure it includes
+            // Atlas cluster specifics. Be sure it includes
             // a valid username and password! Note that in a production environment,
             // you do not want to store your password in plain-text here.
 
